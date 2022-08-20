b9aaf1302031dcfe82ae11d0ee5f0a80c90b272d
Mitchell Roger <roger.mitch27@gmail.com>
2022-08-20 22:04:53 +1000

    fix up

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 // this function  loads the nav bar on all the pages
 $(function () {
-  $("#nav-placeholder").load("/src/pages/UI/nav.html");
+  $("#nav-placeholder").load("/src/nav.html");
 });
 
 $(function () {
-  $("#footer-placeholder").load("/src/pages/UI/footer.html");
+  $("#footer-placeholder").load("/src/footer.html");
 });
